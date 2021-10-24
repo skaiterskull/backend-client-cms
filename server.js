8f912ce19afc475acfa9207f38e6b066cfb1a371
Hendra Wijaya <hendra.heree@gmail.com>
2021-10-24 20:46:42 +1100

    little change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import mongoClient from "./src/config/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 mongoClient();
 
 app.use(cors());
